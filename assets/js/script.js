b31f16d6ac2cdb2241cb9cdb167a4a821b6604ec
lokesh <lokesh.nemade30@gmail.com>
2016-11-27 19:23:07 +0530

    styling of lp is completed

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(".ham").click(function(){
+    $(".resp-menu").animate({
+        width: 'toggle'
+    });
+});
+
+$(".resp-menu-div").click(function(){
+    $(".resp-menu").animate({
+        width: 'toggle'
+    });
+});
